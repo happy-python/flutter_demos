d8bfc6502b4a070e0d1ff6faec0f50885722af09
jack <1091354206@qq.com>
2018-09-20 13:32:03 +0800

    login_ui

--- login_ui/lib/main.dart
+++ login_ui/lib/main.dart
@@ -5,6 +5,7 @@ import 'login_page.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
+  // 定义路由
   final Map<String, WidgetBuilder> routes = {
     "/": (BuildContext context) => LoginPage(),
     "/home": (BuildContext context) => HomePage(),
